34ce643a7438911977d1101b9f1c8dcd5108b83a
Nirwanto Maingkolang <nirwanto.maingkolang@amarbank.co.id>
2018-02-13 17:15:13 +0700

    Cleaning DiffUtilCallback

--- mvpbasearch/src/main/java/com/scoproject/mvpbasearch/adapter/DiffUtilCallback.kt
+++ mvpbasearch/src/main/java/com/scoproject/mvpbasearch/adapter/DiffUtilCallback.kt
@@ -8,8 +8,7 @@ import android.support.v7.util.DiffUtil
  */
 
 internal class DiffUtilCallback<ITEM>(private val oldItems: List<ITEM>,
-                                      private val newItems: List<ITEM>)
-    : DiffUtil.Callback() {
+                                      private val newItems: List<ITEM>) : DiffUtil.Callback() {
 
     override fun getOldListSize(): Int {
         return oldItems.size
